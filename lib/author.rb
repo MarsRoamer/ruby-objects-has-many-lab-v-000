90ae34a66c9b25a0330192844c30136f32972379
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-10 18:03:15 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,7 +14,13 @@ class Author
     @posts << post
     post.author = self
   end
-    
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @post << post
+    post.author = self
+  end
+
 
 
 
