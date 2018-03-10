e55cc64e7d98e59172df4a6ebe93e612ed235ce5
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-10 18:05:07 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 class Author
   attr_accessor :name
-
+  @@post_count = 0
   def initialize(name)
     @name = name
     @posts = []
@@ -13,16 +13,18 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
-
-
-
+  def self.post_count
+    @@post_count
+  end 
 
 end
